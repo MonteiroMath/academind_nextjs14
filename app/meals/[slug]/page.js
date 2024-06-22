712016315d029f66ea33023b7326f8fb7144ac18
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-06-22 11:01:17 -0300

    Meal Details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,37 @@
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+import Image from "next/image";
+
 function MealsSlugPage({ params }) {
   const { slug } = params;
 
-  return <main> {slug} </main>;
+  const meal = getMeal(slug);
+
+  const parsedInstructions = meal.instructions.replace(/\n/g, "<br/>");
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt="meal image" fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}> {meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: parsedInstructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
 
 export default MealsSlugPage;
