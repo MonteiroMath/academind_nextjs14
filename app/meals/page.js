a43acd370644dceb20199373ffd1bd762a55ce1a
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-06-22 09:55:21 -0300

    Meals page, meals item, meals grid

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,29 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
+import MealsGrid from "@/components/meals/meals-grid";
+
 function MealsPage() {
-  return <main> Meals Page</main>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+          <p>
+            Choose your favorite recipe and cook it yourself. It is easy and
+            fun!
+          </p>
+          <p className={styles.cta}>
+            <Link href="/meals/share">Share Your Favorite Recipe</Link>
+          </p>
+        </h1>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
 
 export default MealsPage;
