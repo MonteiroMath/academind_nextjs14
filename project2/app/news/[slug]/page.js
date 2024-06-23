288e42f296d8c1a6a7ef9a0a23fa8e9b96f92f1e
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-06-23 14:51:19 -0300

    nested route

--- project2/app/news/[slug]/page.js
+++ project2/app/news/[slug]/page.js
@@ -1,4 +1,5 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 function NewsDetailPage({ params }) {
@@ -13,7 +14,10 @@ function NewsDetailPage({ params }) {
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link href={`/news/${newsItem.slug}/image`}>
+          {" "}
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
         <h1>{newsItem.title}</h1>
         <time dateTime={newsItem.date}>{newsItem.date}</time>
         <p>{newsItem.content}</p>
