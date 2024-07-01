b763546057cc057c93dac5996c4965b01f66fcfd
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-01 10:41:00 -0300

    navigation - pages router

--- project7/pages/clients/index.js
+++ project7/pages/clients/index.js
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    {
+      id: "max",
+      name: "Maximilian",
+    },
+    {
+      id: "manu",
+      name: "Manuel",
+    },
+    {
+      id: "jose",
+      name: "Joseph",
+    },
+  ];
+
   return (
     <div>
       <h1>ClientsPage</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
