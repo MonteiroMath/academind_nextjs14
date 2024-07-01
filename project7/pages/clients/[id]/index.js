cd0db64afc1f8fe00ada49b622111c461d871399
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-01 10:50:14 -0300

    programmatic navigation - pages routes

--- project7/pages/clients/[id]/index.js
+++ project7/pages/clients/[id]/index.js
@@ -1,7 +1,18 @@
+import { useRouter } from "next/router";
+
 function ClientProjectsPage() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    // do stuff...
+
+    router.push(`/clients/max/projecta`);
+  }
+
   return (
     <div>
       <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
