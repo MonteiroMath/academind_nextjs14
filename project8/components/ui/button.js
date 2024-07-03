ff9dfdbf8a3fba78a73ba2b4279814a22cc53ece
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-03 09:17:19 -0300

    buttons and icons - pages router

---
+++ project8/components/ui/button.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import classes from "./button.module.css";
+
+function Button({ link, children }) {
+  return (
+    <Link className={classes.btn} href={link}>
+      {children}
+    </Link>
+  );
+}
+
+export default Button;
