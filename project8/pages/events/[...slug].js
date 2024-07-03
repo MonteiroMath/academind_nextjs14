2a9caac094e2f8de6c6091888a340c9061527297
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-03 10:13:36 -0300

    filter event functionality

--- project8/pages/events/[...slug].js
+++ project8/pages/events/[...slug].js
@@ -1,11 +1,32 @@
 import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+
+import EventList from "../../components/events/event-list";
 
 function FilteredEventsPage() {
   const router = useRouter();
 
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const [year, month] = filterData;
+
+  if (isNaN(+year) || isNaN(+month)) {
+    return <p>INvalid filter. Plase adjust your values!</p>;
+  }
+
+  const events = getFilteredEvents({ year: +year, month: +month });
+
+  if (!events || events.length === 0) {
+    return <p>No events found</p>;
+  }
+
   return (
     <div>
-      <h1>Filtered Events</h1>
+      <EventList items={events} />
     </div>
   );
 }
