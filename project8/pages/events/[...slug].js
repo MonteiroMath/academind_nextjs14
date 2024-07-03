6264ad6152e0f4f4f658c22033ac969186fc6645
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-03 10:27:48 -0300

    final styles

--- project8/pages/events/[...slug].js
+++ project8/pages/events/[...slug].js
@@ -1,7 +1,10 @@
 import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../dummy-data";
 
+import ResultsTitle from "../../components/results-title/results-title";
 import EventList from "../../components/events/event-list";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/events/error-alert";
 
 function FilteredEventsPage() {
   const router = useRouter();
@@ -9,7 +12,16 @@ function FilteredEventsPage() {
   const filterData = router.query.slug;
 
   if (!filterData) {
-    return <p className="center">Loading...</p>;
+    return (
+      <>
+        <ErrorAlert>
+          <p className="center">Loading...</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all events</Button>
+        </div>
+      </>
+    );
   }
 
   const [year, month] = filterData;
@@ -21,11 +33,23 @@ function FilteredEventsPage() {
   const events = getFilteredEvents({ year: +year, month: +month });
 
   if (!events || events.length === 0) {
-    return <p>No events found</p>;
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all events</Button>
+        </div>
+      </>
+    );
   }
+
+  const date = new Date(+year, +month - 1);
 
   return (
     <div>
+      <ResultsTitle date={date} />
       <EventList items={events} />
     </div>
   );
