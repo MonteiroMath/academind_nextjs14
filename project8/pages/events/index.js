7faef3c459b6bbc4f29c355831a34f394eabde26
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-03 09:38:18 -0300

    all events pages

--- project8/pages/events/index.js
+++ project8/pages/events/index.js
@@ -1,5 +1,15 @@
+import EventList from "../../components/events/event-list";
+
+import { getAllEvents } from "../../dummy-data";
+
 function AllEventsPage() {
-  return <div>Events Pages</div>;
+  const events = getAllEvents();
+
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
 }
 
 export default AllEventsPage;
