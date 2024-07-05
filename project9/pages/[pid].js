03022a84a94e57ce00f6d944086a8200bd020dc7
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-05 09:57:11 -0300

    dynamic loading of paths

--- project9/pages/[pid].js
+++ project9/pages/[pid].js
@@ -13,13 +13,23 @@ function ProductDetailPage({ loadedProduct }) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+
+  const params = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-     
-    ],
-    fallback: true,
+    paths: params,
+    fallback: "blocking",
   };
 }
 
@@ -27,10 +37,7 @@ export async function getStaticProps(context) {
   const { params } = context;
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
