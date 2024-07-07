566df8448f59a2e049cb71667f9a8376f1099177
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-07 10:19:52 -0300

    static site generation on the home page"

--- project10/pages/index.js
+++ project10/pages/index.js
@@ -1,14 +1,24 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from "../helpers/api-utils";
+import EventList from "../components/events/event-list";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage({ featuredEvents }) {
+  //const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
       <EventList items={featuredEvents} />
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      featuredEvents,
+    },
+  };
 }
 
 export default HomePage;
