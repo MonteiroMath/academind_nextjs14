874167c680fd97bb3e0f7da2315b30eb6f59517d
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-07 10:42:53 -0300

    static generation for 'All Events' page

--- project10/pages/events/index.js
+++ project10/pages/events/index.js
@@ -1,17 +1,14 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 
-import { getAllEvents } from '../../dummy-data';
-import EventList from '../../components/events/event-list';
-import EventsSearch from '../../components/events/events-search';
+import { getAllEvents } from "../../helpers/api-utils";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
-function AllEventsPage() {
-  const router = useRouter();
-  const events = getAllEvents();
-
+function AllEventsPage({ events }) {
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
-    
+
     router.push(fullPath);
   }
 
@@ -21,6 +18,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEventsPage;
