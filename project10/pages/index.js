87e011b8ae3ef89da65b4941c17695cc36d9ba2d
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-08 09:58:16 -0300

    metadata - pages router

--- project10/pages/index.js
+++ project10/pages/index.js
@@ -1,11 +1,14 @@
+import Head from "next/head";
 import { getFeaturedEvents } from "../helpers/api-utils";
 import EventList from "../components/events/event-list";
 
 function HomePage({ featuredEvents }) {
-  //const featuredEvents = getFeaturedEvents();
-
   return (
     <div>
+      <Head>
+        <title>NextJS events</title>
+        <meta name="description" content="Find a lot of great events that allow you to evolve..."/>
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
