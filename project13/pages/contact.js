6c23fe791f6a6b9be991bdbdc4853d50d70e242d
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-11 12:23:47 -0300

    Contact form

--- project13/pages/contact.js
+++ project13/pages/contact.js
@@ -1,5 +1,6 @@
+import ContactForm from "../components/contact/contact-form";
 function ContactPage() {
-  return <div> Contact Page </div>;
+  return <ContactForm/>;
 }
 
 export default ContactPage;
