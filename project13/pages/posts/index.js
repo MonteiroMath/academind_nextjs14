12574d80708cc1c526950b97493864326ad90a80
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-11 10:10:12 -0300

    post detail page

--- project13/pages/posts/index.js
+++ project13/pages/posts/index.js
@@ -1,41 +1,43 @@
 import Hero from "../../components/home-page/hero";
 import AllPosts from "../../components/posts/all-posts";
 
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for proudction - It makes building fullstack react app easier",
+    date: "2024-09-10",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for proudction - It makes building fullstack react app easier",
+    date: "2024-09-10",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for proudction - It makes building fullstack react app easier",
+    date: "2024-09-10",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is the React framework for proudction - It makes building fullstack react app easier",
+    date: "2024-09-10",
+  },
+];
+
 function AllPostsPage() {
-  const DUMMY_POSTS = [
-    {
-      slug: "getting-started-with-nextjs",
-      title: "Getting started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for proudction - It makes building fullstack react app easier",
-      date: "2024-09-10",
-    },
-    {
-      slug: "getting-started-with-nextjs2",
-      title: "Getting started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for proudction - It makes building fullstack react app easier",
-      date: "2024-09-10",
-    },
-    {
-      slug: "getting-started-with-nextjs3",
-      title: "Getting started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for proudction - It makes building fullstack react app easier",
-      date: "2024-09-10",
-    },
-    {
-      slug: "getting-started-with-nextjs4",
-      title: "Getting started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for proudction - It makes building fullstack react app easier",
-      date: "2024-09-10",
-    },
-  ];
+
 
   return (
     <>
